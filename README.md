5b872beb194517aee0dfaf82132e50b4997fd802
sheodox <3468630+sheodox@users.noreply.github.com>
2021-09-08 20:42:09 -0500

    Updated the theme to use colors from Ayu, while keeping the usages of this theme. To make changing themes easier I made a new JSON file (sheodoxdark.json) that gets colors replaces with a script, then imported/exported on Pinto to generate the vim scripkt.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sheodoxdark
 
-This is a vim theme based originally on Blackboard.
+This is a vim theme based on Blackboard and Ayu.
 
 Most of the customization of this color scheme has been done using [Pinto](https://pintovim.dev/) and the theme can be customized by importing the `pinto-sheodoxdark.json` file in the root of the repository.
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs/promises'),
+	colorsFilePath = './sheodoxdark.json',
+	outputFilePath = './pinto-sheodoxdark.json',
+	colors = [
+		['identifier', 'ff8f40'],
+		['parameter', '399ee6'],
+		['keyword', 'ffb454'],
+		['string', 'aad94c'],
+		['comment', '636a72'],
+		['normal', 'e5e1cf'],
+		['number', '39bae6'],
+		['regexp', '94e6cb'],
+		['error', 'f07178'],
+		['todo', '6c5980'],
+		['tag', '39bae6'],
+		['symbol', 'd2a6ff'],
+		['variable', '5ccfe6'],
+		['static', '979748'],
+		['normalBackground', '0e1419'],
+		['colorColumn', '444b55'],
+		['cursorLine', '131721'],
+		['selection', '1b3a5b'],
+	],
+	colorMapping = {};
+
+// turn each color identifier into a more unique string surrounded in <<>>
+colors.forEach(([colorKey, color]) => {
+	colorMapping[`<<${colorKey}>>`] = `#${color}`;
+});
+
+
+async function build() {
+	const colorsWithKeywords = JSON.parse(await fs.readFile(colorsFilePath));
+	
+
+	for (const keywordName of Object.keys(colorsWithKeywords.theme.keywords)) {
+		const keyword = colorsWithKeywords.theme.keywords[keywordName],
+			bg = keyword.backgroundColor,
+			fg = keyword.foregroundColor;
+
+		if (colorMapping[bg]) {
+			keyword.backgroundColor = colorMapping[bg];
+		}
+		if (colorMapping[fg]) {
+			keyword.foregroundColor = colorMapping[fg];
+		}
+	}
+
+	await fs.writeFile(outputFilePath, JSON.stringify(colorsWithKeywords));
+}
+
+build().catch(e => console.error(e));
